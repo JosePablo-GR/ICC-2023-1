30d831217f5df7ed29a5434ddc0ca6ba96d07c85
José Pablo GR <89881634+JosePablo-GR@users.noreply.github.com>
2022-09-09 23:27:56 -0500

    Update JuegoDados.java

--- Practica03/JuegoDados.java
+++ Practica03/JuegoDados.java
@@ -5,24 +5,24 @@ public class Dadittos{
 
 	public static void main (String [] args) {
 
-		Scanner lector = new Scanner(System.in);
-		Random aleatorios = new Random();
+	Scanner lector = new Scanner(System.in);
+	Random aleatorios = new Random();
 
-		int dadoUno; 
-		int dadoDos;
-		int puntos = 0;
-		int sumaDados = 0;
-		boolean centinela = true;
+	int dadoUno; 
+	int dadoDos;
+	int puntos = 0;
+	int sumaDados = 0;
+	boolean centinela = true;
 
-		System.out.println("Este programa simula un juego de dados");
+	System.out.println("Este programa simula un juego de dados");
 
-		System.out.println("Escribe tu nombre: ");
+	System.out.println("Escribe tu nombre: ");
 
-		String nombre = lector.nextLine();
-		System.out.println("\nCada vez que quieras tirar escribe 'tirar'");
+	String nombre = lector.nextLine();
+	System.out.println("\nCada vez que quieras tirar escribe 'tirar'");
 
-		System.out.println("Empieza a tirar: ");
-		String tirada = lector.nextLine().toLowerCase();	
+	System.out.println("Empieza a tirar: ");
+	String tirada = lector.nextLine().toLowerCase();	
 
 		if (tirada.equals("tirar")){
 
@@ -34,63 +34,64 @@ public class Dadittos{
 
 			sumaDados = dadoUno + dadoDos;
 
-			if (sumaDados == 7 || sumaDados == 11){
+		if (sumaDados == 7 || sumaDados == 11){
 
 					System.out.println();
 					System.out.println(nombre + " ha GANADO");
 
-				} else if (sumaDados == 2 || sumaDados == 3 || sumaDados == 12){
+			} else if (sumaDados == 2 || sumaDados == 3 || sumaDados == 12){
 
 					System.out.println("\ncraps: " + nombre + " ha PERDIDO, la casa GANA");
 
 				} else {
 
-					puntos += sumaDados;
-					System.out.println("\nPor ende, se convierten en puntos, tienes que tirar hasta llegar a una suma de " + puntos);
-					System.out.println("**Empieza la cuenta de tus turnos**");
+				puntos += sumaDados;
+				System.out.println("\nPor ende, se convierten en puntos, tienes que tirar hasta llegar a una suma de " + puntos);
+				System.out.println("**Empieza la cuenta de tus turnos**");
 
-					sumaDados = 0;
+				sumaDados = 0;
 
-					//System.out.println("PUNTOOOS A LLEGAR: " + puntos);
-					//System.out.println("SUUUMA DADOS: " + sumaDados);
+				//System.out.println("PUNTOOOS A LLEGAR: " + puntos);
+				//System.out.println("SUUUMA DADOS: " + sumaDados);
 
-					//System.out.println("Se ejecuta solo si entra a puntos");
+				//System.out.println("Se ejecuta solo si entra a puntos");
 
-					while(centinela){
+			while(centinela){
 
-						System.out.println("\nSigue tirando, quieres llegar al " + puntos);
-						tirada = lector.nextLine().toLowerCase();
-						System.out.println();
-						//System.out.println("Este es tu intento num. " + turno);
+				System.out.println("\nSigue tirando, quieres llegar al " + puntos);
+				tirada = lector.nextLine().toLowerCase();
+				System.out.println();
+				//System.out.println("Este es tu intento num. " + turno);
 
-						if (tirada.equals("tirar")){
+				if (tirada.equals("tirar")){
 
-							dadoUno = aleatorios.nextInt(6)+1;
-							dadoDos = aleatorios.nextInt(6)+1;
+					dadoUno = aleatorios.nextInt(6)+1;
+					dadoDos = aleatorios.nextInt(6)+1;
 
-							System.out.println("El primer dado te dio: " + dadoUno);
-							System.out.println("El segundo dado te dio: " + dadoDos);
+						System.out.println("El primer dado te dio: " + dadoUno);
+						System.out.println("El segundo dado te dio: " + dadoDos);
 
-							sumaDados = dadoUno + dadoDos;
+				sumaDados = dadoUno + dadoDos;
 
-						}//Fin if tirar
+					}//Fin if tirar
 
-						if (sumaDados == 7){
+			if (sumaDados == 7){
 
-							centinela = false;
+				centinela = false;
 
-						System.out.println();
-						System.out.println(nombre + " ha PERDIDO porque salió suma de " + sumaDados + " :(");
+				System.out.println();
+				System.out.println(nombre + " ha PERDIDO porque salió suma de " + sumaDados + " :(");
 
-					} else if (sumaDados == puntos){
+			} else if (sumaDados == puntos){
 
-						centinela = false;
-						System.out.println();
-						System.out.println(nombre + " ha GANADO por llegar a la suma de " + puntos + "!!");
-
-					} //Fin gans o pierdes
-
-					} //Fin centinela 
+				centinela = false;
+				System.out.println();
+				System.out.println(nombre + " ha GANADO por llegar a la suma de " + puntos + "!!");
+							
+							
+	} //Fin gans o pierdes
+						
+} //Fin centinela 
 
 
 				} //Fin if-ELSE sumas y empieza puntos
