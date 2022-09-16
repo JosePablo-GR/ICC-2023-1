7180f19ce98c2a0a18fcd717a333a1a974b24990
José Pablo GR <89881634+JosePablo-GR@users.noreply.github.com>
2022-09-16 15:26:18 -0500

    Forsitos

--- Practica03/Rombito.java
+++ Practica03/Rombito.java
@@ -4,12 +4,49 @@ public class Rombito{
 
 	public static void main (String [] args){
 
-		Scanner lector = new Scanner(System.in);
+	Scanner lector = new Scanner(System.in);
 
-		System.out.println("Este programa imprime un rombo con 'diametro menor' igual a un número ingresado del teclado");
-		System.out.println("Por favor igresa el número: ");
+	System.out.println("Este programa imprime un rombo con 'diametro menor' igual a un número ingresado del teclado");
+	System.out.println("Por favor igresa el número: ");
 
 		int n = lector.nextInt();
+		
+		System.out.println("\nRombo: \n");
+
+		for(int i = 0; i <= n; i++){
+
+			for(int j = n-i; j > 0; j--){
+
+				System.out.print(" ");
+
+			}
+
+			for(int k = 0; k < i; k++){
+
+				System.out.print(" *");
+
+			}
+
+			System.out.println("");
+
+		}//Fin for ida
+
+		for(int i = 0; i <= n; i++){
+
+			for(int j = 0; j <= i; j++){
+
+				System.out.print(" ");
+			}
+
+			for(int k = n-i-1; k > 0; k--){
+
+				System.out.print(" *");
+
+			}
+
+			System.out.println("");
+
+		}//Fin for espejo
 
 		
 
