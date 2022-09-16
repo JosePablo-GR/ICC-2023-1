4bbe1a87dd811983afd34ac26d97f7366da53d00
José Pablo GR <89881634+JosePablo-GR@users.noreply.github.com>
2022-09-15 23:44:16 -0500

    Create Rombito.java

---
+++ Practica03/Rombito.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Rombito{
+
+	public static void main (String [] args){
+
+		Scanner lector = new Scanner(System.in);
+
+		System.out.println("Este programa imprime un rombo con 'diametro menor' igual a un número ingresado del teclado");
+		System.out.println("Por favor igresa el número: ");
+
+		int n = lector.nextInt();
+
+		
+
+	}//Fin main
+
+}//Fin class
