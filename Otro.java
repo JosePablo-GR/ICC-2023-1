07cba00d0d00136f8433b040c53d88c4bd931e55
José Pablo GR <89881634+JosePablo-GR@users.noreply.github.com>
2022-09-20 08:36:19 -0500

    Add files via upload

---
+++ Otro.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Otro{
+
+	public static void main(String[] args){
+
+		String usuario;
+		boolean sePudo = true;
+		Scanner entrada = new Scanner(System.in);
+
+		System.out.println("Hola! Para ingresar a tus datos escribe tu usuario");
+
+		System.out.println("Usuario: ");
+		usuario = entrada.next();
+
+
+		if(!(usuario.equals("JosePablo"))){
+
+			while (sePudo){
+
+			System.out.println("\nUsuario inválido, intente de nuevo: ");
+
+			usuario = entrada.next();
+
+
+			if(usuario.equals("JosePablo")){
+
+				sePudo = false;
+				System.out.println("\nBienvenido a tus datos, " + usuario);
+
+				}//Fin ya no estás en while
+
+			}//Fin while
+
+		}//Fin otra vez
+
+	}//Fin main
+
+}//Fin class
