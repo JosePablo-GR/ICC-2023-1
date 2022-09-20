ee72c3f55e688a1bc587afa5456b4e8540351bc5
José Pablo GR <89881634+JosePablo-GR@users.noreply.github.com>
2022-09-20 08:37:38 -0500

    Cambio de nombre

--- Usuario.java
+++ Usuario.java
@@ -1,17 +1,17 @@
 import java.util.Scanner;
 
-public class Otro{
+public class Usuario{
 
 	public static void main(String[] args){
 
-		String usuario;
-		boolean sePudo = true;
-		Scanner entrada = new Scanner(System.in);
+	String usuario;
+	boolean sePudo = true;
+	Scanner entrada = new Scanner(System.in);
 
-		System.out.println("Hola! Para ingresar a tus datos escribe tu usuario");
+	System.out.println("Hola! Para ingresar a tus datos escribe tu usuario");
 
-		System.out.println("Usuario: ");
-		usuario = entrada.next();
+	System.out.println("Usuario: ");
+	usuario = entrada.next();
 
 
 		if(!(usuario.equals("JosePablo"))){
