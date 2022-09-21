151de0e5353fe36ea4451021685c1d34e08872b8
José Pablo GR <89881634+JosePablo-GR@users.noreply.github.com>
2022-09-21 08:18:57 -0500

    Update Canicas.java

--- Proyecto01/Canicas.java
+++ Proyecto01/Canicas.java
@@ -10,35 +10,37 @@ public class Canicas{
 		char pregunta;
 		int jugador;
 		int compu;
+		int questComp;
 		int apuestaJugador;
 		int apuestaCompu;
 		boolean par;
 		boolean impar;
+		boolean seRepite;
+		boolean error;
 
 		aleatorio = new Random();
 		lector = new Scanner(System.in);
-
-		//System.out.println("Escoge");
 
 		System.out.println("Acabo de repartir 10 canicas para la computadora y 10 para Miguel");
 
 		jugador = 10;
 		compu = 10;
+		seRepite = true;
 
-		System.out.println("Miguel, ¿cuántas canicas quieres apostar?");
+		while(seRepite){
+
+		/////////Nosotros adivinamos/////////
+		System.out.println("\nMiguel, es tu turno de adivinar\n¿cuántas canicas quieres apostar?");
+		System.out.println("Tienes " + jugador + " canicas");
 
 		apuestaJugador = lector.nextInt();
+
 		jugador -= apuestaJugador;
 
-		System.out.println("\nJugador tiene " + jugador + " canicas");
-
-		System.out.println("\nLa computadora eligirá cuántas canicas apostará: ");
+		System.out.println("\nLa computadora eligirá cuántas canicas apostará...");
 
 		apuestaCompu = aleatorio.nextInt(compu)+1;
 		compu -= apuestaCompu;
-
-		System.out.println("Computadora apostó " + apuestaCompu + " canicas");
-		//System.out.println("Computadora tiene " + compu + " canicas");
 
 		if(apuestaCompu % 2 == 0){
 
@@ -62,18 +64,74 @@ public class Canicas{
 			System.out.println("\nAdivinaste!");
 			jugador += apuestaCompu + apuestaJugador;
 			
-			System.out.println("Ahora tienes " + jugador + " canicas :)");
-			System.out.println("Computadora tiene " + compu + " canicas");
+			System.out.println("Te quedarás con las canicas que apostó la computadora");
 
 		} else {
 
 			System.out.println("\nErraste");
 			compu += apuestaCompu + apuestaJugador;
 
-			System.out.println("Ahora tienes " + jugador + " canicas :(");
-			System.out.println("Computadora tiene " + compu + " canicas");
+			System.out.println("Ahora la computadora tiene las canicas que apostaste");
 
-		}//Fin cuantás TÚ apuestas	
+		}//Fin cuantás TÚ apuestas
+
+		/////////Computadora adivina/////////
+		System.out.println("\nMiguel, es turno de la computadora a adivinar\n¿cuántas canicas quieres apostar?");
+		System.out.println("\nLa computadora adivinará si la cantidad que tienes es par o impar");
+		System.out.println("Tienes " + jugador + " canicas");
+
+		apuestaJugador = lector.nextInt();
+		jugador -= apuestaJugador;
+
+		//System.out.println("\nLa computadora eligirá cuántas canicas apostará...");
+
+		apuestaCompu = aleatorio.nextInt(compu)+1;
+		compu -= apuestaCompu;
+
+		if(apuestaJugador % 2 == 0){
+
+			par = true;
+			impar = false;
+
+		} else {
+
+			impar = true;
+			par = false;
+
+		}//Fin parImpar
+
+		questComp = aleatorio.nextInt(2);
+
+		if(par && questComp == 0 || impar && questComp == 1){
+
+			System.out.println("Computadora dice que es par, ADIVINÓ");
+			compu += apuestaCompu + apuestaJugador;
+			
+			System.out.println("Ahora tienes " + jugador + " canicas :(");
+
+		} else {
+
+			System.out.println("Computadora dice que es impar, FALLÓ");
+			jugador += apuestaCompu + apuestaJugador;
+
+			System.out.println("Ahora tienes " + jugador + " canicas :)");
+
+		}//Fin adivina compu
+
+		////////////NO TOQUES////////////
+		if(jugador == 20){
+				seRepite = false;
+
+				System.out.println("Ganaste por llegar a la cantidad de " + jugador + " canicas! :p");
+
+			}else if(compu == 20){
+				seRepite = false;
+
+				System.out.println("Computadora ganó por llegar a la cantidad de " + compu + " canicas! :(");	
+
+			}//Fin termina juego
+
+		}//Fin while juego
 
 	}//Fin main
 
