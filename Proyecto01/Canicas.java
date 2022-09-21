8d00e18b79bbf519ad8bbb66f80c57fcdb3902ba
José Pablo GR <89881634+JosePablo-GR@users.noreply.github.com>
2022-09-20 20:49:19 -0500

    Update Canicas.java

--- Proyecto01/Canicas.java
+++ Proyecto01/Canicas.java
@@ -10,33 +10,70 @@ public class Canicas{
 		char pregunta;
 		int jugador;
 		int compu;
-		int cuantas;
+		int apuestaJugador;
+		int apuestaCompu;
+		boolean par;
+		boolean impar;
 
 		aleatorio = new Random();
 		lector = new Scanner(System.in);
 
-		System.out.println("Acabo de repartir 10 canicas para la computadora y para Miguel");
+		//System.out.println("Escoge");
+
+		System.out.println("Acabo de repartir 10 canicas para la computadora y 10 para Miguel");
 
 		jugador = 10;
 		compu = 10;
 
 		System.out.println("Miguel, ¿cuántas canicas quieres apostar?");
 
-		cuantas = lector.nextInt();
-		jugador -= cuantas;
+		apuestaJugador = lector.nextInt();
+		jugador -= apuestaJugador;
 
 		System.out.println("\nJugador tiene " + jugador + " canicas");
 
 		System.out.println("\nLa computadora eligirá cuántas canicas apostará: ");
 
-		cuantas = aleatorio.nextInt(10)+1;
-		compu -= cuantas;
+		apuestaCompu = aleatorio.nextInt(10)+1;
+		compu -= apuestaCompu;
 
-		System.out.println("Computadora apostó " + cuantas);
-		System.out.println("\nComputadora tiene " + compu + " canicas");
+		System.out.println("Computadora apostó " + apuestaCompu + " canicas");
+		//System.out.println("Computadora tiene " + compu + " canicas");
 
-		System.out.println("Miguel, ¿crees que la cantidad que apostó la computadora es par o impar?");
+		if(apuestaCompu % 2 == 0){
+
+			par = true;
+			impar = false;
+
+		} else {
+
+			impar = true;
+			par = false;
+
+		}//Fin parImpar
+
+		System.out.println("\nMiguel, ¿crees que la cantidad que apostó la computadora es par o impar?");
 		System.out.println("Escribe 'P' para par o 'I' para impar");
+
+		pregunta = lector.next().toUpperCase().charAt(0);
+
+		if(par && pregunta == 'P' || impar && pregunta == 'I'){
+
+			System.out.println("\nAdivinaste!");
+			jugador += apuestaCompu + apuestaJugador;
+			
+			System.out.println("Ahora tienes " + jugador + " canicas :)");
+			System.out.println("Computadora tiene " + compu + " canicas");
+
+		} else {
+
+			System.out.println("\nErraste");
+			compu += apuestaCompu + apuestaJugador;
+
+			System.out.println("Ahora tienes " + jugador + " canicas :(");
+			System.out.println("Computadora tiene " + compu + " canicas");
+
+		}//Fin cuantás TÚ apuestas	
 
 	}//Fin main
 
