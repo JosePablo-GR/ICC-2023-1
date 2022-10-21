0491c706d677740966d5d6e52bf60a176e0ff1c6
José Pablo GR <89881634+JosePablo-GR@users.noreply.github.com>
2022-10-20 21:30:11 -0500

    Create TallerAuto.java

---
+++ Practica04/Act2/TallerAuto.java
@@ -0,0 +1,17 @@
+class TallerAuto{
+
+	boolean encendido;
+
+	public void setEncendido(boolean encendido){
+
+		this.encendido = encendido;
+	}
+
+	public boolean getEncendido(){
+
+		return encendido;
+	}
+
+	
+
+}//Fin class
