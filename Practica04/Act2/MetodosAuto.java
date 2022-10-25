67cf9070927abc782b37c854510ff9cd2a9c4ea9
José Pablo GR <89881634+JosePablo-GR@users.noreply.github.com>
2022-10-25 16:42:41 -0500

    Update MetodosAuto.java

--- Practica04/Act2/MetodosAuto.java
+++ Practica04/Act2/MetodosAuto.java
@@ -90,7 +90,7 @@ public class MetodosAuto{
 
 				System.out.println("\nNos quedamos en el kilometro " + j);
 				System.out.println("No logramos completar el viaje");
-				System.out.println("El auto se apagará...");
+				System.out.println("El auto se apagará...\n");
 				setEncendido(false);
 
 			}//Fin if nos quedamos sin gasolina
@@ -101,8 +101,21 @@ public class MetodosAuto{
 
 	public void cargarGasolinaDinero(double dinero){
 
-		Scanner entrada = new Scanner(System.in);
 		Random aleatorios = new Random();
+		int multiplo = 1;
+		int teSaldraEn = 0;
+
+		for(int cont = 1; cont <= 10; cont++){
+
+			if(this.gasolinaActual % 9.18 == 0){
+
+				System.out.println(multiplo + " es multiplo de 9.18");
+
+			} else {
+
+				System.out.println(multiplo + " no es múltiplo de 9.18");
+
+			}
 
 		int n = aleatorios.nextInt(5)+1;
 		System.out.println();
@@ -110,17 +123,126 @@ public class MetodosAuto{
 		for(int x = 1; x <= n; x++){
 
 			System.out.println("Cargando gasolina...");
-		}
 
-		while(dinero > 0.00){
+		}//Fin whie imprime mensaje xD
+
+		}//Fin for recargas gasolina
+
+		System.out.println("Por favor serán $" + teSaldraEn);
+
+	}//Fin método cargarGasolinaDinero
+
+	public void cargarGasolinaLitros(int litros){
+
+		litros = (int) litros;
+
+		double cuantoSera = litros*aCuantoLaGas; //En $$
+
+		for(int i = 0; i <= litros; i++){
 
 			this.gasolinaActual++;
 
-			if(){}
+		}//Fin for cargando gasolina
 
-		}//Fin while recargas gasolina
+		System.out.println("De tus " + litros + " van a ser $" + cuantoSera);
 
-	}//Fin método cargarGasolinaDinero
+	}//Fin método cargarGasolinaLitros
+
+	public void cargarTodoElTanque(){
+
+		int resta = 0;
+
+		if(this.gasolinaActual < 1){
+
+			System.out.println("Te faltaban unas gotas para llenar el tanque");
+			System.out.println("Ntp, nosotros lo invitamos");
+
+		} else {
+
+			for(int i = 0; i <= capacidadTotal; i++){
+
+				
+
+
+			}//Fin for rellena gasolina
+
+			setGasolinaActual(50.00);
+
+		}//Fin if cuánto de gasolina faltaba
+
+	}//Fin método cargarTodoElTanque
+
+	public void carreritas(String a1, String a2){
+
+		Random aleatorios = new Random();
+
+		int vueltasC1 = 0; //Contador Carro 1
+		int vueltasC2 = 0; //Contador Carro 2
+
+		System.out.println();
+		System.out.println();
+		System.out.println("                                   Inicia la carrera!!!");
+
+		for(int vueltas = 1; vueltas <= 10; vueltas++){
+
+			int confusion = aleatorios.nextInt(4)+1;
+			boolean cc1 = false; //Confusión Carro 1
+			boolean cc2 = false; //Confusión Carro 2
+
+		if(confusion == 1){
+
+			cc1 = true;
+
+		} else if(confusion == 4){
+
+			cc2 = true;
+
+		}//Fin a ver si hay confusión
+
+		if(cc1){
+
+			System.out.println(a1 + " se detiene porque el conductor ha entrado en confusión");
+
+			if(vueltasC1 > 0){
+
+				vueltasC1--;
+
+			}//Fin if  aver si no es 0
+
+			vueltasC2++;
+			System.out.println(a2 + " ha completado " + vueltasC2 + " vueltas");
+
+		}//Fin if Carro 1 tiene confusión
+
+		if(cc2){
+
+			System.out.println(a2 + " se detiene porque el conductor ha entrado en confusión");
+
+			if(vueltasC2 > 0){
+
+				vueltasC2--;
+
+			}//Fin if  aver si no es 0
+
+			vueltasC1++;
+			System.out.println(a1 + " ha completado " + vueltasC1 + " vueltas");
+
+		}//Fin if Carro 2 tiene confusión
+
+		 if(cc1 == false && cc2 == false){
+
+			vueltasC1++;
+			vueltasC2++;
+			System.out.println(a1 + " ha completado " + vueltasC1 + " vueltas");
+			System.out.println(a2 + " ha completado " + vueltasC2 + " vueltas");
+
+		}//Fin if imprime si se confunsión o sigue
+
+		System.out.println("*****************************************************************************");
+
+		}//while for vueltas
+
+	}//Fin método carreritas
 
 	public String toString(){
 
