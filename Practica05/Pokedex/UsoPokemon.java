60d7d3118e980fae478ef56a47cc0e04f331f536
José Pablo GR <89881634+JosePablo-GR@users.noreply.github.com>
2022-11-04 21:48:14 -0600

    Update UsoPokemon.java

--- Practica05/Pokedex/UsoPokemon.java
+++ Practica05/Pokedex/UsoPokemon.java
@@ -14,7 +14,24 @@ class UsoPokemon{
 		System.out.println("de los cuales alguno nos puede envenenar, unos con un toque gélido e incluso");
 		System.out.println("hay otros que nos pueden quemar.\n");
 
-	}//Fin contexto 
+	}//Fin contexto
+
+	public static void quienPoke(String pokemonComparacion){
+
+		Pokemon pokemoncito = new Pokemon();
+
+		if(pokemoncito.pokemonEncontrado().equals(pokemonComparacion)){
+
+			System.out.println("Vaya, somos... nosotros!! :o");
+			System.out.println("Claro.. Somos tan populares que hicieron un clon nuestro");
+
+		} else {
+
+			System.out.println("Vaya, es " + pokemoncito.pokemonEncontrado() + "!");
+
+		}//Fin if somos el mismo
+
+	}//Fin método quienPoke
 
 	public static void main(String[] args){
 
@@ -25,7 +42,6 @@ class UsoPokemon{
 		boolean yaElegiste = false;
 
 		System.out.println("Bienvenido a Build-A-Poke");
-		System.out.println("Existen más de 15 tipos de Pokemón, aquí solo manejamos 10");
 		System.out.println("Selecciona algún tipo para ver los pokemones de ese tipo, luego escogerás");
 
 		while(!yaElegiste){
@@ -40,6 +56,14 @@ class UsoPokemon{
 		System.out.println("8. Roca");
 		System.out.println("9. Tierra");
 		System.out.println("10. Volador");
+		System.out.println("11. Agua");
+		System.out.println("12. Hielo");
+		System.out.println("13. Planta");
+		System.out.println("14. Bicho");
+		System.out.println("15. Fuego");
+		System.out.println("16. Hada");
+		System.out.println("17. Veneno");
+		System.out.println("18. Siniestro");
 
 		int queTipo = entrada.nextInt();
 
@@ -105,6 +129,49 @@ class UsoPokemon{
 
 			break;
 
+		case 11: 
+
+			System.out.println("Squirtle, Psyduck, Poliwrath, Seel, Magikarp");
+
+			break;
+
+		case 12:
+
+			System.out.println("Snom, Cetitan, Glalie, Jynx, Bergmite");
+
+			break;
+
+		case 13:
+
+			System.out.println("Bellsprout, Oddish, Gloom, Cherrim, Rillaboom");
+
+			break;
+
+		case 14:
+
+			System.out.println("Paras, Caterpie, Venemoth, Shelmet, Dotteler");
+			break;
+
+		case 15:
+
+			System.out.println("Charmander, Vulpix, Torchic, Numel, Tepig");
+			break;
+
+		case 16:
+
+			System.out.println("Fodough, Clefairy, Togepi, Floette, Spritzee");
+			break;
+
+		case 17:
+
+			System.out.println("Ekans, Arbok, Zubat, Grimer, Gulpin");
+			break;
+
+		case 18:
+
+			System.out.println("Murkrow, Absol, Vullaby, Deino, Inkay");
+			break;
+
 		default:
 
 			System.out.println("Esa opción no coincide");
@@ -162,7 +229,7 @@ class UsoPokemon{
 			tipo = "Fantasma";
 			loHizoBn = true;
 
-		}else if(quePoke.equals("machap") || quePoke.equals("pancham") ||
+		}else if(quePoke.equals("machamp") || quePoke.equals("pancham") ||
 		quePoke.equals("mankey") || quePoke.equals("clobbopus") ||
 		quePoke.equals("falinks")){
 
@@ -203,6 +270,54 @@ class UsoPokemon{
 			tipo = "Volador";
 			loHizoBn = true;
 
+		}else if(quePoke.equals("squirtle") || quePoke.equals("psyduck") ||
+		quePoke.equals("poliwrath") || quePoke.equals("seel") || quePoke.equals("magikarp")){
+
+			tipo = "Agua";
+			loHizoBn = true;
+
+		}else if(quePoke.equals("snom") || quePoke.equals("cetitan") ||
+		quePoke.equals("glalie") || quePoke.equals("jynx") || quePoke.equals("bergmite")){
+
+			tipo = "Hielo";
+			loHizoBn = true;
+
+		}else if(quePoke.equals("bellsprout") || quePoke.equals("oddish") ||
+		quePoke.equals("gloom") || quePoke.equals("cherrim") || quePoke.equals("rillaboom")){
+
+			tipo = "Planta";
+			loHizoBn = true;
+
+		}else if(quePoke.equals("paras") || quePoke.equals("caterpie") ||
+		quePoke.equals("venomoth") || quePoke.equals("shelmet") || quePoke.equals("dottler")){
+
+			tipo = "Bicho";
+			loHizoBn = true;
+
+		}else if(quePoke.equals("charmander") || quePoke.equals("vulpix") ||
+		quePoke.equals("torchic") || quePoke.equals("numel") || quePoke.equals("tepig")){
+
+			tipo = "Fuego";
+			loHizoBn = true;
+
+		}else if(quePoke.equals("fodough") || quePoke.equals("clefairy") ||
+		quePoke.equals("togepi") || quePoke.equals("floette") || quePoke.equals("spritzee")){
+
+			tipo = "Hada";
+			loHizoBn = true;
+
+		}else if(quePoke.equals("ekans") || quePoke.equals("arbok") ||
+		quePoke.equals("zubat") || quePoke.equals("grimer") || quePoke.equals("gulpin")){
+
+			tipo = "Veneno";
+			loHizoBn = true;
+
+		}else if(quePoke.equals("murkrow") || quePoke.equals("absol") ||
+		quePoke.equals("vullaby") || quePoke.equals("deino") || quePoke.equals("inkay")){
+
+			tipo = "Siniestro";
+			loHizoBn = true;
+
 		} else {
 
 			System.out.println();
@@ -218,6 +333,7 @@ class UsoPokemon{
 		System.out.println();
 		Pokemon pokemoncito = new Pokemon(quePoke, tipo);
 		pokemoncito.setHP(100);
+		int getAchePe = pokemoncito.getHP();
 
 		System.out.println(pokemoncito);
 		System.out.println("\nRecuerda que cada pokemón viene con una bolsita que contiene: ");
@@ -226,7 +342,7 @@ class UsoPokemon{
 		pokemoncito.setBolsitaOran(true);
 		pokemoncito.setBolsitaFullH(true);
 
-		int tantasVueltas = aleatorios.nextInt(5)+1;
+		int tantasVueltas = aleatorios.nextInt(10)+1;
 
 		System.out.println("\nMuy bien, ahora lo que haremos es dar un paseo por el Bosque Lumirinto");
 		System.out.println("¿Quieres información sobre este? 1 = sí / 2 = omitir");
@@ -246,7 +362,7 @@ class UsoPokemon{
 		System.out.println("¿Cuántas vueltas quieres dar? Para un pokemón así recomiendan " + tantasVueltas + " o más ¯|_(ツ)_|¯");
 		int vueltas = entrada.nextInt();
 
-		int medioCamino = vueltas/2; 
+		double medioCamino = (int) vueltas/2; 
 
 		if(vueltas < tantasVueltas){
 
@@ -258,16 +374,40 @@ class UsoPokemon{
 
 		}//Fin if vueltas
 
-		for(int i = 0; i <= vueltas; i++){
+		int casualidades;
 
-			int casualidades = aleatorios.nextInt(6)+1;
+		while(vueltas >= 1){
 
-			System.out.println("\nCaminando...  Tu HP es de " + pokemoncito.getHP());
+			vueltas--;
+
+			pokemoncito.checaHP(pokemoncito.getHP());
+			//pokemoncito.checaHP(pokemoncito.getHP());
+			casualidades = aleatorios.nextInt(6)+1;
+
+			if(casualidades == 2){
+
+				pokemoncito.probabilidadEnfermarse(quePoke);
+
+			} else if(casualidades == 4){
+
+				pokemoncito.caerse(quePoke);
+
+			} else if(casualidades == 6){
+
+				pokemoncito.comparaPokemones(tipo);
+				quienPoke(quePoke);
+
+			} else {
+
+				System.out.println("\nCaminando...  Tu HP es de " + pokemoncito.getHP());
+
+			}//Fin if qué problemas
 
 			if(vueltas == medioCamino){
 
-					System.out.println("\n¿Cómo ves, quieres usar algo de tu bolsita? [S/N]");
-					siOno = entrada.next().toUpperCase().charAt(0);
+				System.out.println("\nMitad del camino, nos faltan " + vueltas + " vueltas");
+				System.out.println("¿Cómo ves, quieres usar algo de tu bolsita? [S/N]");
+				siOno = entrada.next().toUpperCase().charAt(0);
 
 					if(siOno == 'S'){
 
@@ -276,42 +416,24 @@ class UsoPokemon{
 
 						if(opcion == 1){
 
-							pokemoncito.comerOran();
+							pokemoncito.comerOran(pokemoncito.getHP());
 							pokemoncito.setBolsitaOran(false);
 
 						} else if(opcion == 2) {
 
 							pokemoncito.fuljil();
+							System.out.println("Ya estamos, listos para para seguir:)");
 							pokemoncito.setBolsitaFullH(false);
 
 						}//Fin if qué usará
 
 					}//Fin if bolsita
 
-				}//Fin if mitad del camino
+				}//Fin if else mitadCamino
 
-				if(casualidades == 2){
+			}//Fin while caminando
 
-				pokemoncito.probabilidadEnfermarse(quePoke);
-
-				} else if(casualidades == 4){
-
-				pokemoncito.caerse(quePoke);
-
-				} else if(casualidades == 6){
-
-				pokemoncito.comparaPokemones(quePoke);
-
-				}//Fin if qué problemas
-
-				if(i > vueltas){
-
-					break;
-				//i = vueltas;
-
-			}//Fin if terminalo
-
-		}//Fin for caminando
+			System.out.println("\nTus vueltas han acabado :v");
 
 		}//Fin if loHizoBn
 
